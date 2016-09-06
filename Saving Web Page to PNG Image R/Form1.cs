b57ef64a3f17d7b7c27a9f25a71d9670fea4eda1
vladislav.tsvek <vladislav.tsvek@V-TSVEK>
2016-09-06 12:27:29 +0300

    Add project files.

---
+++ Saving Web Page to PNG Image R/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saving_Web_Page_to_PNG_Image_R
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
